01d263396d3ea89349936112c3075c58f30271a6
saeedkhalilabadi <saeedkhalilabadi@gmail.com>
2023-05-09 03:26:09 +0430

    add router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import routes from "./router";
+
+
+const getRoutes = (allRoutes) =>
+  allRoutes.map((route) => {
+    if (route.route) {
+      return <Route path={route.route} Component={route.component} key={route.key} />;
+    }
+    return null;
+  });
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Suspense >
+        <BrowserRouter>
+          <Routes>
+            {getRoutes(routes)}
+            <Route path="*" element={<Navigate to="/flightSearch" replace />} />
+          </Routes>
+        </BrowserRouter>
+      </Suspense >
     </div>
   );
 }
---
+++ src/router.js
@@ -0,0 +1,18 @@
+
+
+import FlightSearch from "./layouts/flightSearch";
+
+
+const routes = [
+  {
+    type: "collapse",
+    name: "FlightSearch",
+    key: "FlightSearch",
+    route: "/FlightSearch",
+    component: FlightSearch,
+    noCollapse: true,
+  },
+]
+
+export default routes;
+
